a4d77f00bd6c910a76445daf2c9034a81611498d
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2022-11-15 19:26:33 +0400

    Updates

---
+++ precache-manifest.bf0a042c0076158054f89d6e34fa0e89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca807a230289b0e9d03b",
+    "url": "/DigitalBusinessCard/static/css/main.7884a9b8.chunk.css"
+  },
+  {
+    "revision": "ca807a230289b0e9d03b",
+    "url": "/DigitalBusinessCard/static/js/main.ca807a23.chunk.js"
+  },
+  {
+    "revision": "f98f0c76faea84607e83",
+    "url": "/DigitalBusinessCard/static/js/1.f98f0c76.chunk.js"
+  },
+  {
+    "revision": "d92039dc7264ad83817b",
+    "url": "/DigitalBusinessCard/static/js/runtime~main.d92039dc.js"
+  },
+  {
+    "revision": "8a8fe1ae1432075f2f826d12fadccc65",
+    "url": "/DigitalBusinessCard/static/media/face.8a8fe1ae.png"
+  },
+  {
+    "revision": "9fd7c6bb311664c4d97f9316fba4c5e7",
+    "url": "/DigitalBusinessCard/static/media/behance.9fd7c6bb.png"
+  },
+  {
+    "revision": "b226a0ba2dfde7dc9af6592b674ad2a4",
+    "url": "/DigitalBusinessCard/static/media/telegram_white.b226a0ba.png"
+  },
+  {
+    "revision": "3d90e13be469d854f09dce9194b1ee00",
+    "url": "/DigitalBusinessCard/index.html"
+  }
+];
