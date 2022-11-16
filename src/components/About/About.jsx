15575e483c8c352697abf9d9a0c7799eb3d13726
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2022-11-16 15:11:06 +0400

    Changed text in About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='about__container'>
       <h3>How I can help you</h3>
       <p>I am an aspiring designer/developer with a background in creative coding and generative art.</p> 
-      <p>I specialize in scalable and flexible brand identities made with code, fresh web experiences and generative art prints and installations.</p>
+      <p>I specialize in flexible brand identities made with code, web experiences, art prints and installations.</p>
     </div>
   )
 }
